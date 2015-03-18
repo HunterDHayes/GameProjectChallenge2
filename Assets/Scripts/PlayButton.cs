65b2a8d843b862556b5000b229fd38e4b07b6bfd
William Kennedy <kennedyalec@aol.com>
2015-03-18 12:46:30 -0400

    Added Gameplay.unity and button in main menu to switch to it.

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour {
+
+	public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void SwitchToGameplay()
+	{
+		Application.LoadLevel(levelToLoad)	;
+	}
+}
