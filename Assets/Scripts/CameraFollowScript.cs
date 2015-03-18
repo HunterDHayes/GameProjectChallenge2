d422f58bbdee8d538012ea11db1dfd031bab3e40
William Kennedy <kennedyalec@aol.com>
2015-03-18 13:23:03 -0400

    Adding meta files and camera follow script.

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+
+	private GameObject player = null;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!player) player = GameObject.FindGameObjectWithTag("Player");
+		if(player)
+		{
+			
+		}
+	}
+}
