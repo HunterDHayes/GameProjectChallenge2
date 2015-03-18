fb1a417c8ebdbed4c908f63bc6e842af78ea1e6d
William Kennedy <kennedyalec@aol.com>
2015-03-18 19:27:55 -0400

    Meters counter.

--- Assets/Scripts/LevelScrollerScript.cs
+++ Assets/Scripts/LevelScrollerScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class LevelScrollerScript : MonoBehaviour {
 
@@ -11,6 +12,9 @@ public class LevelScrollerScript : MonoBehaviour {
 	public float timer = 0.0f;
 	public float speedUpTime = 30.0f;
 	public float maxScrollSpeed = 15.0f;
+	
+	public GameObject metersText;
+	private float metersCounter = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -27,12 +31,15 @@ public class LevelScrollerScript : MonoBehaviour {
 			scrollSpeed *= 1.05f;
 			scrollSpeed = Mathf.Min(scrollSpeed, this.maxScrollSpeed);
 		}
-
+		
+		metersCounter += scrollSpeed * Time.deltaTime;
+		
 		for(int i = 0; i < transform.childCount; i++)
 		{
 			transform.GetChild(i).position -= new Vector3(scrollSpeed * Time.deltaTime, 0.0f, 0.0f);
 		}
-
+		
+		metersText.GetComponent<Text>().text = metersCounter.ToString("#0");
 
 
 		//Check if need to spawn new floor
