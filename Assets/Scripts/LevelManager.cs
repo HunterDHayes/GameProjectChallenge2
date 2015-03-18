385e4e4882309ef792034966b93cf866802c9458
wes <thebest1288@fullsail.edu>
2015-03-18 17:39:17 -0400

    MOD changed the scroll functionality

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,31 +4,59 @@ using System.Collections;
 public class LevelManager : MonoBehaviour 
 {
 	public GameObject[] terrainBlocks;
+	public int numBlocks = 0;
 
 	private GameObject currentBlock;
 	private GameObject nextBlock;
-
-	private int blockCounter = 0;
+	public float xOffsetPerTile = 0.0f;
+	public float yOffsetPerTile = 0.0f;
+	private int blockCounter = 1;
 
 	// Use this for initialization
 	void Start () 
 	{
-		this.currentBlock = Instantiate (terrainBlocks [0], new Vector3(0.0f, -7.5f, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
-		this.nextBlock = Instantiate (terrainBlocks [1], new Vector3(50.0f, -7.5f, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
+		currentBlock = Instantiate (terrainBlocks [0], new Vector3(0.0f, yOffsetPerTile, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
+		nextBlock = Instantiate (terrainBlocks [1], new Vector3(xOffsetPerTile, yOffsetPerTile, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
+
+		currentBlock.transform.SetParent (transform);
+		nextBlock.transform.SetParent (transform);
+		blockCounter = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (this.currentBlock.transform.position.x < -50.0f) 
+		int tilenum = blockCounter % 2;
+
+		GameObject blockToCheck;
+		if (tilenum == 0) 
 		{
-			Destroy (this.currentBlock);
+			blockToCheck = currentBlock;
+		} 
+		else 
+		{
+			blockToCheck = nextBlock;
+		}
+	
 
-			this.currentBlock = this.nextBlock;
+		if (blockToCheck.transform.position.x <= -(xOffsetPerTile)) 
+		{
+			blockCounter++;
+			int visibletile = blockCounter % 2;
 
-			this.nextBlock = Instantiate (terrainBlocks [blockCounter++], new Vector3(this.currentBlock.transform.position.x + 50.0f, -7.5f, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
+			if(visibletile == 0)
+			{
+				Destroy(currentBlock);
+				currentBlock = Instantiate (terrainBlocks [blockCounter % numBlocks], new Vector3(xOffsetPerTile, yOffsetPerTile, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
+				currentBlock.transform.SetParent(transform);
+			}
+			else
+			{
+				Destroy(nextBlock);
+				nextBlock = Instantiate (terrainBlocks [blockCounter % numBlocks], new Vector3(xOffsetPerTile, yOffsetPerTile, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
+				nextBlock.transform.SetParent(transform);
+			}
 
-			this.blockCounter = this.blockCounter % 2;
 		}
 	}
 }
