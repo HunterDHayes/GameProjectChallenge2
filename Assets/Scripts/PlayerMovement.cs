f144b27ad1ef51a4a52db4da5ec1b3df7f508af0
Mitchell Martin <MLMatin275@gmail.com>
2015-03-18 18:20:40 -0400

    Player KnockBack

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,13 +9,24 @@ public class PlayerMovement : MonoBehaviour {
 	
 	private float currJumpTime = 0.0f;
 
+	public float knockBackCount = 3;
+	public float currKnockBack = 0;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {	
+	void Update () 
+	{
+		Vector3 position = this.transform.position;
+		position.x = Mathf.Lerp (this.transform.position.x, (-7.5f) * (this.currKnockBack / this.knockBackCount), Time.deltaTime * 2.0f);
+		this.transform.position = position;
+
+		if (Input.GetKeyDown (KeyCode.K))
+			this.KnockBack ();
+
 		float dY = Input.GetAxis("Vertical");
 		
 		if(currJumpTime > 0.0f){
@@ -34,6 +45,15 @@ public class PlayerMovement : MonoBehaviour {
 				currJumpTime = jumpTime;
 				GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, dY * jumpImpulse), ForceMode2D.Impulse);
 			}
+		}
+	}
+
+	public void KnockBack()
+	{
+		this.currKnockBack++;
+
+		if (this.currKnockBack >= this.knockBackCount) {
+			// TODO Do death stuff here
 		}
 	}
 }
