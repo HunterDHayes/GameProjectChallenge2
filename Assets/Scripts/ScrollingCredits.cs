910ed0d16dafeb3bfd93f1f88bec74d3a35e69db
Mitchell Martin <MLMatin275@gmail.com>
2015-03-19 18:25:47 -0400

    Added scrolling credits.

---
+++ Assets/Scripts/ScrollingCredits.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingCredits : MonoBehaviour {
+
+	public GameObject image;
+	public int startPoint;
+	public int endPoint;
+	public float scrollSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 position = image.transform.position;
+		position.y += Time.deltaTime * this.scrollSpeed;
+
+		if (position.y >= this.endPoint)
+			position.y = this.startPoint;
+
+		image.transform.position = position;
+	}
+}
