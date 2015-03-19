1c7dbe363c8ee6b441c40a0a465616828c421f23
wes <thebest1288@fullsail.edu>
2015-03-18 20:17:03 -0400

    MOD: updated the world scrolling

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,16 +8,15 @@ public class LevelManager : MonoBehaviour
 
 	private GameObject currentBlock;
 	private GameObject nextBlock;
-	public float xOffsetPerTile = 0.0f;
-	public float yOffsetPerTile = 0.0f;
+	public GameObject spawnPoint = null;
 	private int blockCounter = 1;
 
 	// Use this for initialization
 	void Start () 
 	{
 
-		currentBlock = Instantiate (terrainBlocks [0], new Vector3(0.0f, yOffsetPerTile, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
-		nextBlock = Instantiate (terrainBlocks [1], new Vector3(terrainBlocks [1].transform.localScale.x, yOffsetPerTile, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
+		currentBlock = Instantiate (terrainBlocks [0], new Vector3(0.0f, 0.0f, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
+		nextBlock = Instantiate (terrainBlocks [1], spawnPoint.transform.position, new Quaternion(0, 0, 0, 0)) as GameObject;
 
 		currentBlock.transform.SetParent (transform);
 		nextBlock.transform.SetParent (transform);
@@ -27,10 +26,8 @@ public class LevelManager : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		int tilenum = blockCounter % 2;
-
 		GameObject blockToCheck;
-		if (tilenum == 0) 
+		if (blockCounter % 2 == 0) 
 		{
 			blockToCheck = currentBlock;
 		} 
@@ -38,26 +35,37 @@ public class LevelManager : MonoBehaviour
 		{
 			blockToCheck = nextBlock;
 		}
-	
 
-		if (blockToCheck.transform.position.x <= -(blockToCheck.transform.localScale.x)) 
+		if (blockToCheck.transform.position.x <= 0.0f) 
 		{
 			blockCounter++;
 			int visibletile = blockCounter % 2;
 
 			if(visibletile == 0)
 			{
-				Destroy(currentBlock);
-				currentBlock = Instantiate (terrainBlocks [blockCounter % numBlocks], new Vector3(terrainBlocks [blockCounter % numBlocks].transform.localScale.x, yOffsetPerTile, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
+
+				GameObject toDestroy = currentBlock;
+				currentBlock = Instantiate (terrainBlocks [blockCounter % numBlocks], spawnPoint.transform.position, new Quaternion(0, 0, 0, 0)) as GameObject;
 				currentBlock.transform.SetParent(transform);
+				Vector3 pos = spawnPoint.transform.position;
+				pos.x -= .2f;
+				currentBlock.transform.position = pos;
+
+				Destroy(toDestroy);
 			}
 			else
 			{
-				Destroy(nextBlock);
-				nextBlock = Instantiate (terrainBlocks [blockCounter % numBlocks], new Vector3(terrainBlocks [blockCounter % numBlocks].transform.localScale.x, yOffsetPerTile, 0.0f), new Quaternion(0, 0, 0, 0)) as GameObject;
+				GameObject toDestroy = nextBlock;
+				nextBlock = Instantiate (terrainBlocks [blockCounter % numBlocks], spawnPoint.transform.position, new Quaternion(0, 0, 0, 0)) as GameObject;
 				nextBlock.transform.SetParent(transform);
+				Vector3 pos = spawnPoint.transform.position;
+				pos.x -= .2f;
+				nextBlock.transform.position = pos;
+				Destroy(toDestroy);
 			}
 
 		}
+
+
 	}
 }
